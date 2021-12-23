e95a4893d9584c0446b74512ec82c9b87c42033f
Daniel Payares <danielpayaresl@gmail.com>
2021-12-22 22:26:23 -0300

    Don't stop me now, cuz i'm having a good time, having a good time.

--- nasa/views.py
+++ nasa/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index (request):
     return HttpResponse("Nasa APP")
+
+def specific(request):
+    return HttpResponse("This is the specific URL")
--- nasa_api/settings.py
+++ nasa_api/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+=8kwgr4^dugnk=5iw4=pv2gy(0%uiz^l+h#&bb!sio&cqz_7m'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
